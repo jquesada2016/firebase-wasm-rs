4c6a9c24d86a56f056d958402ba48681e57ec6fb
Jose Quesada <jquesada2016@fau.edu>
2022-09-03 11:51:48 -0500

    added all storage metadata fields and updated deps

--- src/storage/bindings.rs
+++ src/storage/bindings.rs
@@ -6,6 +6,12 @@ extern "C" {
     pub type Ref;
     pub type UploadTask;
     pub type UploadTaskSnapshot;
+    pub type SettableMetadata;
+    #[wasm_bindgen(extends = SettableMetadata)]
+    pub type UploadMetadata;
+    #[wasm_bindgen(extends = UploadMetadata)]
+    pub type FullMetadata;
+    pub type TaskState;
 
     #[wasm_bindgen(js_name = getStorage)]
     pub fn get_storage() -> Storage;
@@ -14,10 +20,7 @@ extern "C" {
     pub fn ref_(storage: Storage, path: &str) -> Ref;
 
     #[wasm_bindgen(js_name = uploadBytesResumable, catch)]
-    pub fn upload_bytes(
-        ref_: Ref,
-        data: &web_sys::Blob,
-    ) -> Result<UploadTask, JsValue>;
+    pub fn upload_bytes(ref_: Ref, data: &web_sys::Blob) -> Result<UploadTask, JsValue>;
 
     #[wasm_bindgen(js_name = getDownloadURL, catch)]
     pub async fn get_download_url(ref_: Ref) -> Result<JsValue, JsValue>;
@@ -60,6 +63,78 @@ extern "C" {
     #[wasm_bindgen(method, getter, js_name = totalBytes)]
     pub fn total_bytes(this: &UploadTaskSnapshot) -> usize;
 
-    #[wasm_bindgen(method, getter, js_name = "ref")]
+    #[wasm_bindgen(method, getter, js_name = ref)]
     pub fn ref_(this: &UploadTaskSnapshot) -> Ref;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn metadata(this: &UploadTaskSnapshot) -> FullMetadata;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn state(this: &UploadTaskSnapshot) -> String;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn task(this: &UploadTaskSnapshot) -> UploadTask;
+
+    // =========================================================================
+    //                            SettableMetadata
+    // =========================================================================
+
+    #[wasm_bindgen(method, getter, js_name = cacheControl)]
+    pub fn cache_control(this: &SettableMetadata) -> Option<String>;
+
+    #[wasm_bindgen(method, getter, js_name = contentDisposition)]
+    pub fn content_disposition(this: &SettableMetadata) -> Option<String>;
+
+    #[wasm_bindgen(method, getter, js_name = contentEncoding)]
+    pub fn content_encoding(this: &SettableMetadata) -> Option<String>;
+
+    #[wasm_bindgen(method, getter, js_name = contentLanguage)]
+    pub fn content_language(this: &SettableMetadata) -> Option<String>;
+
+    #[wasm_bindgen(method, getter, js_name = contentType)]
+    pub fn content_type(this: &SettableMetadata) -> Option<String>;
+
+    #[wasm_bindgen(method, getter, js_name = customMetadata)]
+    pub fn custom_metadata(this: &SettableMetadata) -> Option<js_sys::Object>;
+
+    // =========================================================================
+    //                            UploadMetadata
+    // =========================================================================
+
+    #[wasm_bindgen(method, getter, js_name = md5Hash)]
+    pub fn md5_hash(this: &UploadMetadata) -> Option<String>;
+
+    // =========================================================================
+    //                            FullMetadata
+    // =========================================================================
+
+    #[wasm_bindgen(method, getter)]
+    pub fn bucket(this: &FullMetadata) -> String;
+
+    #[wasm_bindgen(method, getter, js_name = fullPath)]
+    pub fn full_path(this: &FullMetadata) -> String;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn generation(this: &FullMetadata) -> String;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn metageneration(this: &FullMetadata) -> String;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn name(this: &FullMetadata) -> String;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn size(this: &FullMetadata) -> u64;
+
+    #[wasm_bindgen(method, getter, js_name = timeCreated)]
+    pub fn time_created(this: &FullMetadata) -> String;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn updated(this: &FullMetadata) -> String;
+
+    #[wasm_bindgen(method, getter, js_name = downloadTokens)]
+    pub fn download_tokens(this: &FullMetadata) -> Vec<js_sys::JsString>;
+
+    #[wasm_bindgen(method, getter, js_name = ref)]
+    pub fn ref_(this: &FullMetadata) -> Option<Ref>;
 }
