d6ec69b80fbb5dbe7306abe15009e3f751491038
Fredrik Norén <fredrik.jw.noren@gmail.com>
2023-07-25 15:41:58 +0200

    Expose add_doc

--- src/firestore.rs
+++ src/firestore.rs
@@ -3,9 +3,9 @@ mod bindings;
 use crate::FirebaseError;
 use bindings as b;
 pub use bindings::{
-    delete_doc, doc, get_firestore, limit, on_snapshot_doc, on_snapshot_query, query, set_doc,
-    update_doc, CollectionReference, DocumentReference, DocumentSnapshot, Firestore, Query,
-    QueryConstraint, QuerySnapshot, SetDocOptions, Timestamp, Transaction,
+    add_doc, delete_doc, doc, get_firestore, limit, on_snapshot_doc, on_snapshot_query, query,
+    set_doc, update_doc, CollectionReference, DocumentReference, DocumentSnapshot, Firestore,
+    Query, QueryConstraint, QuerySnapshot, SetDocOptions, Timestamp, Transaction,
 };
 use futures::Future;
 use std::{cell::RefCell, error::Error, fmt, rc::Rc, time::SystemTime};
